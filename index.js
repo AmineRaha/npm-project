c5219240d90e1ad18e3432bb80f2aed886866938
AmineRaha <amineraha2@gmail.com>
2022-11-26 18:15:00 +0100

    1.0.2

---
+++ index.js
@@ -0,0 +1,17 @@
+function  add(a = float, b = float){
+    return a+b;
+}
+
+function  substract(a = float, b = float){
+    return a-b;
+}
+
+function  multiply(a = float, b = float){
+    return a*b;
+}
+
+function  divide(a = float, b = float){
+    return a/b;
+}
+
+module.exports = {add, substract, multiply, divide};
